cc60abcfb2b2b702f90dc0bb8cc3c0b5458bda8b
Paul Evans <pauljevans927@gmail.com>
2021-12-26 22:52:51 -0500

    Added Schoology URLS file

---
+++ scraper/schoology/urls.py
@@ -0,0 +1,6 @@
+# Schoology URLs (constants)
+# No need to make them secrets
+
+SCHOOLOGY_LOGIN_URL = "https://hackley.schoology.com/login/ldap?&school=1938279719"
+SCHOOLOGY_URL = "https://hackley.schoology.com"
+SCHOOLOGY_IAPI2_URL = "https://hackley.schoology.com/iapi2/site-navigation/courses?includeAll=1"
