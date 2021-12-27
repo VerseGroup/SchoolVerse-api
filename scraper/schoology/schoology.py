747b19174e335b01eab68e3c2971a154caf76a6f
Paul Evans <pauljevans927@gmail.com>
2021-12-26 23:00:21 -0500

    Todo list/Scraper Login/Docs

--- scraper/schoology/schoology.py
+++ scraper/schoology/schoology.py
@@ -1,3 +1,10 @@
+# This file contains the function that scrapes schoology assignments/courses using a username/passeord
+
+# Todo #
+# - Add checks in case login failed 
+# - Add a lot more try/excepts for other errors
+# - Finish copying over parsers, scrapers, etc from SchoolVerse testing
+
 # internal packages
 import os
 import sys
@@ -23,5 +30,13 @@ from urls import SCHOOLOGY_URL, SCHOOLOGY_LOGIN_URL, SCHOOLOGY_IAPI2_URL
 # and outputs the JSON formatted courses/tasks associated with that username
 def scrape_schoology(username, password):
     
-    # the URL session
+    # the URL session to scrape from
     s = requests.Session()
+
+    # login fields
+    login_params = f"mail={username}&pass={password}&school_nid=1938279719&form_id=s_user_login_form"
+    login_headers =  {"Content-Type" : "application/x-www-form-urlencoded"}
+
+    # login post request
+    response = s.post(url=SCHOOLOGY_LOGIN_URL, data=login_params, headers=login_headers)
+    print(f"Login Status: {response.status_code}")  
