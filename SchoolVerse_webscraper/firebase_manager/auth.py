6abc2b642dd44548922e52215f6a49212143079a
Paul Evans <pauljevans927@gmail.com>
2021-12-30 16:43:15 -0500

    firebase admin SDK auth

---
+++ SchoolVerse_webscraper/firebase_manager/auth.py
@@ -0,0 +1,20 @@
+# python imports
+import os
+
+# firebase imports
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# secrets
+from dotenv import load_dotenv
+load_dotenv()
+FILE_NAME = os.getenv("FILE_NAME")
+
+# authenticate firebase admin SDK
+def auth():
+    cred = credentials.Certificate(f'SchoolVerse_webscraper/firebase_manager/secrets/{FILE_NAME}')
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    return db
+
