2cf9dd60e1a295ef0b751bf13226c1fc95ea2502
Paul Evans <pauljevans927@gmail.com>
2022-01-04 10:14:34 -0500

    key handler to manage/serialize rsa pub/priv keys

---
+++ SchoolVerse_webscraper/encryption_manager/rsa/key_handler.py
@@ -0,0 +1,52 @@
+import cryptography
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+def generate_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+
+    return {
+        'private_key' : private_key,
+        'public_key' : public_key
+    }
+
+def serialize_keys(private_key, public_key):
+
+    serialized_private_key = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+    )
+
+    serialized_public_key = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return {
+        'serialized_private_key' : serialized_private_key,
+        'serialized_public_key' : serialized_public_key
+    }
+
+def deserialize_keys(serialized_private_key, serialized_public_key):
+    private_key = serialization.load_pem_private_key(
+        serialized_private_key.read(),
+        password=None,
+        backend=default_backend()
+    )
+
+    public_key = serialization.load_pem_public_key(
+        serialized_public_key.read(),
+        backend=default_backend()
+    )
+
+    return {
+        'private_key' : private_key,
+        'public_key' : public_key
+    }
