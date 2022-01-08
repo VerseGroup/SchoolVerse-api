fd1fa1d5a78ee5f10c68b076fd7ba96895ad8c02
Paul Evans <pauljevans927@gmail.com>
2022-01-07 21:18:38 -0500

    starting ss network manager to outsource private keys

--- SchoolVerse_webscraper/keychain_networking/network_manager.py
+++ SchoolVerse_webscraper/keychain_networking/network_manager.py
@@ -3,19 +3,26 @@
 # python imports
 import os
 import sys
+import requests
 
 # adding dir to sys to allow local importing
 currentdir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(currentdir)
+parentdir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(parentdir)
 
 # external imports
 from dotenv import load_dotenv
 
 # gathering secrets
 load_dotenv()
+URL = os.getenv('SS_URL')
 
 # local imports
+from encryption_manager import EncryptionHandler
 
 # function that gets key from security server, decrypts it, and returns credentials
 def get_credentials(user_id, platform_code):
-    pass
+    
+    # a URL Session to manage requests
+    s = requests.Session()
