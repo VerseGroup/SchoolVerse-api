6f85189a36a4b1ee62770ac82b0f9a8656ffb31d
Paul Evans <pauljevans927@gmail.com>
2022-01-09 17:05:40 -0500

    starting veracross scraper

---
+++ SchoolVerse_webscraper/scraper/veracross/scrape_veracross.py
@@ -0,0 +1,19 @@
+# python imports
+import os
+import sys
+import requests
+
+# adding directories for local imports
+parentdir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(parentdir)
+currentdir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(currentdir)
+
+# local imports
+from urls import LOGIN_URL, schedule_url
+
+def scrape_veracross(year, month, day, username, password):
+    
+    s = requests.Session()
+
+    
---
+++ SchoolVerse_webscraper/scraper/veracross/urls.py
@@ -0,0 +1,4 @@
+def schedule_url(year, month, day):
+    return f"https://portals.veracross.com/hackley/student/student/daily-schedule?date={year}-{month}-{day}"
+
+LOGIN_URL="https://accounts.veracross.com/hackley/portals/login"
