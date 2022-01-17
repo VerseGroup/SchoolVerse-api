7a76fd51a16b62f4e241591d187ba944079a5dd6
Paul Evans <pauljevans927@gmail.com>
2022-01-17 14:31:39 -0500

    firebase secrets filename doesn't also have to be secret

--- webscraper/firebase_manager/__init__.py
+++ webscraper/firebase_manager/__init__.py
@@ -7,6 +7,6 @@ currentdir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(currentdir)
 
 # local imports
-from read_encryptions import get_encrypted_credentials
+from read_credentials import get_encrypted_credentials
 from write_tasks import write_task
 from write_credentials import write_creds
--- webscraper/firebase_manager/auth.py
+++ webscraper/firebase_manager/auth.py
@@ -7,13 +7,12 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # secrets
-from dotenv import load_dotenv
-load_dotenv()
-FILE_NAME = os.getenv("FILE_NAME")
+FILE_NAME = 'testing-edacb-firebase-adminsdk-v8h2i-f189331af9.json'
+SECRETS_PATH = f'webscraper/firebase_manager/secrets/{FILE_NAME}'
 
 # authenticate firebase admin SDK
 def auth():
-    cred = credentials.Certificate(f'webscraper/firebase_manager/secrets/{FILE_NAME}')
+    cred = credentials.Certificate(SECRETS_PATH)
     firebase_admin.initialize_app(cred)
     db = firestore.client()   
     return db
