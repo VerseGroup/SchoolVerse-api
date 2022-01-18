060db33ca7f2ba2230a6536b30bb39681ba9e10c
Paul Evans <pauljevans927@gmail.com>
2022-01-18 18:43:03 -0500

    got passed recaptcha, now accesses veracross schedule, parsing next

--- webscraper/scraper/veracross/selenium/scrape_veracross.py
+++ webscraper/scraper/veracross/selenium/scrape_veracross.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import time
+from datetime import date
 
 # adding parent directory to potential package locations 
 parentdir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
@@ -39,13 +40,48 @@ def auth_veracross(username, password):
     password_field = get(driver, By.NAME, 'password')
     password_field.send_keys(password)
 
-    time.sleep(2)
-
     password_field.submit()
-   
-    driver.close()
+
+    time.sleep(1) # seems to fix recaptcha
+
+    # sometimes recaptcha occurs sometimes it doesn't
+    try: 
+        is_login_form = driver.find_element(By.ID, 'username')
+
+        username_field = get(driver, By.NAME, 'username')
+        username_field.send_keys(username)
+
+        password_field = get(driver, By.NAME, 'password')
+        password_field.send_keys(password)
+
+        recpatcha_submit = get(driver, By.ID, 'recaptcha')
+        driver.execute_script("arguments[0].removeAttribute('disabled')", recpatcha_submit)
+        recpatcha_submit.click()
+    except:
+        pass
+
+    return driver
+
+def scrape_schedule(driver, day, month, year):
+
+    SCHEDULE_URL = f"https://portals.veracross.com/hackley/student/student/daily-schedule?date={year}-{month}-{day}"
+    driver.get(SCHEDULE_URL)
+    
 
 if __name__ == '__main__':
     USERNAME = input('USERNAME: ')
     PASSWORD = getpass()
-    auth_veracross(USERNAME, PASSWORD)
+    try:
+        driver = auth_veracross(USERNAME, PASSWORD)
+    except:
+        raise ValueError("Probably didn't enter username or password correctly")
+
+    today = date.today()
+    today = today.strftime("%d/%m/%Y")
+    today = today.split('/')
+
+    scrape_schedule(driver, today[0], today[1], today[2])
+
+
+
+
