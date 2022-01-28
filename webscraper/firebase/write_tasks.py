5125b97ce757231a86c1162ddeb5609d14192fdf
Paul Evans <pauljevans927@gmail.com>
2022-01-27 20:22:16 -0500

    storing task ids in seperate document to reduce reads

--- webscraper/firebase/write_tasks.py
+++ webscraper/firebase/write_tasks.py
@@ -17,20 +17,25 @@ def write_task(task, user_id):
     platform_information = task['platform_information']
     id = task['platform_information']['assignment_code']
 
-    exists = check_task_exists(user_id, id)
+    tasks_ref = db.collection(u'USERS').document(f'{user_id}')
+    tasks_dict = tasks_ref.get().to_dict()
+
+    exists = check_task_exists(user_id, id, tasks_dict)
     
     if not exists:
         db.collection(u'USERS').document(f'{user_id}').collection(u'TASKS').document(f'{id}').set(task)
+        db.collection(u'USERS').document(f'{user_id}').update({"task_ids": tasks_dict['task_ids'] + [id]})
     else:
         print(f"Task {id} already exists")
 
 # iterates through connections to make sure it doesn't overwrite existing data
-def check_task_exists(user_id, id):
-    tasks_ref = db.collection(u'USERS').document(f'{user_id}').collection(u'TASKS')
-    docs = tasks_ref.get()
+def check_task_exists(user_id, id, tasks_dict):
 
-    for doc in docs:
-        if doc.id == id:
+    if tasks_dict is not None:
+        existingids = tasks_dict['task_ids']
+
+    for existingid in existingids:
+        if existingid == id:
             return True
     
     return False
