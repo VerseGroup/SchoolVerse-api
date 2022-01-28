02401f201a96d1445a4cb46e470d6527c6a2b34b
Paul Evans <pauljevans927@gmail.com>
2022-01-27 20:26:26 -0500

    updating task naming conv to users

--- webscraper/firebase/write_tasks.py
+++ webscraper/firebase/write_tasks.py
@@ -14,25 +14,24 @@ from auth import db
 
 # writes a task to firebase within a user collection task collection, after checking that it doens't already exist
 def write_task(task, user_id):
-    platform_information = task['platform_information']
     id = task['platform_information']['assignment_code']
 
-    tasks_ref = db.collection(u'USERS').document(f'{user_id}')
-    tasks_dict = tasks_ref.get().to_dict()
+    user_ref = db.collection(u'USERS').document(f'{user_id}')
+    user_dict = user_ref.get().to_dict()
 
-    exists = check_task_exists(user_id, id, tasks_dict)
+    exists = check_task_exists(user_id, id, user_dict)
     
     if not exists:
         db.collection(u'USERS').document(f'{user_id}').collection(u'TASKS').document(f'{id}').set(task)
-        db.collection(u'USERS').document(f'{user_id}').update({"task_ids": tasks_dict['task_ids'] + [id]})
+        db.collection(u'USERS').document(f'{user_id}').update({"task_ids": user_dict['task_ids'] + [id]})
     else:
         print(f"Task {id} already exists")
 
 # iterates through connections to make sure it doesn't overwrite existing data
-def check_task_exists(user_id, id, tasks_dict):
+def check_task_exists(id, user_dict):
 
-    if tasks_dict is not None:
-        existingids = tasks_dict['task_ids']
+    if user_dict is not None:
+        existingids = user_dict['task_ids']
 
     for existingid in existingids:
         if existingid == id:
