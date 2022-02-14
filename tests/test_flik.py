0a3a9536cf791a31ce74e2dea9ef65b68d7f3e75
Paul Evans <pauljevans927@gmail.com>
2022-02-14 09:27:17 -0500

    added a unit test to test the flik scraper

---
+++ tests/test_flik.py
@@ -0,0 +1,34 @@
+# python imports
+import os
+import sys
+from datetime import date
+
+# adding directories for local imports
+parentdir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(parentdir)
+currentdir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(currentdir)
+
+# imports
+from webscraper.scraper.flik.scraper import scrape_flik
+
+def test_flik(doPrint=False):
+    TYPES = ['breakfast', 'lunch', 'dinner']
+
+    today = date.today()
+    today = today.strftime("%d/%m/%Y")
+    today = today.split('/')
+
+    day = int(today[0])
+    month = int(today[1])
+    year = int(today[2])\
+
+    for type in TYPES:
+        menu = scrape_flik(type, day, month, year)
+        if doPrint:
+            print(menu)
+
+    assert True # testing bugs
+
+if __name__ == '__main__':
+    test_flik(doPrint=True)
