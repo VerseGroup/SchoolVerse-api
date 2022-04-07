0dbaee3487450655ba75df0626f08b7478f58dfb
Paul Evans <pauljevans927@gmail.com>
2022-04-07 14:17:19 -0400

    checking if existing event

--- webscraper/firebase/events.py
+++ webscraper/firebase/events.py
@@ -3,4 +3,12 @@ def write_events(events, db):
         write_event(event, db)
 
 def write_event(event, db):
-    db.collection(u'EVENTS').set(event)
+    if check_event_exists(event, db) == False:
+        db.collection(u'EVENTS').set(event)
+
+def check_event_exists(event, db):
+    current_data = db.collection(u'EVENTS').get().to_dict()
+    for key in current_data:
+        if current_data[key]['platform_information']['vc_id'] == event['platform_information']['vc_id']:
+            return True
+    return False
