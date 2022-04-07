99764a9abbcceb2a2017f72bd92bced5db98a6a6
Paul Evans <pauljevans927@gmail.com>
2022-04-07 15:35:09 -0400

    working event scraper/firebase organization for events

--- webscraper/firebase/events.py
+++ webscraper/firebase/events.py
@@ -8,12 +8,19 @@ def write_event(event, db):
         db.collection(u'EVENTS').document(doc_name).set(event)
 
         existing_events = db.collection(u'EVENTS').document('EXISTING_EVENTS').get().to_dict()['EVENTS']
-        db.collection(u'EVENTS').document("EXISTING_EVENTS").update({"EVENTS": existing_events.append(event['id'])})
+
+        if existing_events is None:
+            existing_events = [event['platform_information']['event_id']]
+        else:
+            existing_events.append(event['platform_information']['event_id'])
+        db.collection(u'EVENTS').document("EXISTING_EVENTS").set({"EVENTS": existing_events})
+    else:
+        print(f"Event {event['id']} already exists")
 
 def check_event_exists(event, db):
     existing_events_dict = db.collection(u'EVENTS').document('EXISTING_EVENTS').get().to_dict()
     existing_events = existing_events_dict['EVENTS']
-    for existing_event in existing_events:
-        if existing_event == event['id']:
+    if existing_events is not None:
+        if event['id'] in existing_events:
             return True
     return False
