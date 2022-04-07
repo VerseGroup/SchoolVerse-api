530dc4c56d9645a1d6102ebb0838cae893a6774b
Paul Evans <pauljevans927@gmail.com>
2022-04-07 15:13:46 -0400

    bug fixing event scraper (vc->event)

--- webscraper/firebase/events.py
+++ webscraper/firebase/events.py
@@ -9,6 +9,6 @@ def write_event(event, db):
 def check_event_exists(event, db):
     current_data = db.collection(u'EVENTS').get().to_dict()
     for key in current_data:
-        if current_data[key]['platform_information']['vc_id'] == event['platform_information']['vc_id']:
+        if current_data[key]['platform_information']['event_id'] == event['platform_information']['event_id']:
             return True
     return False
