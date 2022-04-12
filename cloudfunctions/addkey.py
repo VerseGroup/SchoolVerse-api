8943ebab264ccb853500b75e2a51d7a18fd06ed6
Paul Evans <pauljevans927@gmail.com>
2022-04-11 21:50:59 -0400

    removing dev file

--- cloudfunctions/addkey.py
+++
@@ -1,9 +0,0 @@
-# splits keys into username and password
-
-from vgem import EM
-
-# load username and password from request
-# encrypt both 
-# store ciphers in firebase
-# store keys in security server
-# return status
