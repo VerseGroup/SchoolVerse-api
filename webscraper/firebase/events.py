f3e6ef047a2d18d2e2b250940b838975c60237d3
Paul Evans <pauljevans927@gmail.com>
2022-04-12 16:51:35 -0400

    fixing datetime of events in firebase

--- webscraper/firebase/events.py
+++ webscraper/firebase/events.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 def write_events(events, db):
     for event in events:
@@ -7,21 +7,26 @@ def write_events(events, db):
 def write_event(event, db):
     if check_event_exists(event, db) == False:
         doc_name = f"{event['id']}"
-        db.collection(u'EVENTS').document(doc_name).set(event)
+        
+        start = convert_date(event['start_date'], event['start_time'])
+        end = convert_date(event['end_date'], event['end_time'])
 
+        event.pop('start_date')
+        event.pop('start_time')
+        event.pop('end_date')
+        event.pop('end_time')
+        
+        event['start'] = start
+        event['end'] = end
+
+        db.collection(u'EVENTS').document(doc_name).set(event)
+        
         existing_events = db.collection(u'EVENTS').document('EXISTING_EVENTS').get().to_dict()['EVENTS']
 
         if existing_events is None:
             existing_events = [event['platform_information']['event_id']]
         else:
             existing_events.append(event['platform_information']['event_id'])
-        
-        start = convert_date(event['start_date'], event['start_time'])
-        end = convert_date(event['end_date'], event['end_time'])
-
-        event = event.remove('start_date').remove('start_time').remove('end_date').remove('end_time')
-        event['start'] = start
-        event['end'] = end
         
         db.collection(u'EVENTS').document("EXISTING_EVENTS").set({"EVENTS": existing_events})
     else:
@@ -37,15 +42,17 @@ def check_event_exists(event, db):
 
 def convert_date(date, time):
 
-    if date is None or time is None:
+    if date is None:
         return None
     
-
     date = date.split("/")
     
     month = date[0]
     day = date[1]
     year = date[2]
+
+    if time is None:
+        return datetime(int(year), int(month), int(day))
 
     times = time.split(" ")
     
@@ -58,5 +65,6 @@ def convert_date(date, time):
         hour = int(hour) + 12
 
     date_object = datetime(int(year), int(month), int(day), int(hour), int(minute))
+    
     return date_object
 
