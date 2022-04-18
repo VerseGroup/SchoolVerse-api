dac25115cb0a7cb1414b641fcbe7956251928786
Paul Evans <pauljevans927@gmail.com>
2022-04-18 08:59:27 -0400

    adjusted menu model

--- src/webscraper/firebase/menu.py
+++ src/webscraper/firebase/menu.py
@@ -6,4 +6,5 @@ def write_menu(menu, db):
         menu_ref = db.collection(u'MENUS').document(date)
         to_write = menu[key]
         del to_write['date']
+        to_write = to_write['food']
         menu_ref.set(to_write)
