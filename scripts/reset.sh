bf347f55f0ef563837c9076ddf1adbf2c08e54fb
Paul Evans <pauljevans927@gmail.com>
2022-04-19 14:53:08 -0400

    fixing reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-COMPLETED = "no changes"
+COMPLETED="n/a"
 
 while getopts ":ft:" option; do
     case $option in
         f)
             echo "Option: Reset Flik"
             python3 lib/resets/reset_flik.py
-            COMPLETED = "flik"
+            COMPLETED="flik"
             ;;
         t)
             echo "Option: Reset Tasks"
             python3 lib/resets/reset_tasks.py
-            COMPLETED = "tasks"
+            COMPLETED="tasks"
             ;;
         \?) # Invalid option
             echo "Error: Invalid option"
@@ -22,3 +22,4 @@ while getopts ":ft:" option; do
 done
 
 echo "completed reset: $COMPLETED"
+exit
