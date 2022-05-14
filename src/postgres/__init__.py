d7d2b23c453cc0b8a37e4247e2ff6a84ca288593
Malcolm Krolick <malcolm.krolick@gmail.com>
2022-05-14 19:36:21 -0400

    Removed Key table and associated functions

--- src/postgres/__init__.py
+++ src/postgres/__init__.py
@@ -19,7 +19,8 @@ class Backend_Interface:
         create_user_table_query = """
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
-            firebase_id VARCHAR(255) NOT NULL
+            firebase_id VARCHAR(255) NOT NULL,
+            keychain VARCHAR(3000) NOT NULL
         );
         """
         cursor = self.conn.cursor()
@@ -27,53 +28,24 @@ class Backend_Interface:
         self.conn.commit()
         cursor.close()
 
-    def create_key_table(self):
-        """
-        This function creates a table in the database called users.
-        """
-        create_user_table_query = """
-        CREATE TABLE keys (
-            id SERIAL PRIMARY KEY,
-            platform_code VARCHAR(255) NOT NULL,
-            key VARCHAR(3000) NOT NULL,
-            CONSTRAINT fk_user 
-            FOREIGN KEY(id) 
-            REFERENCES users(id)
-        );
-        """
-        cursor = self.conn.cursor()
-        cursor.execute(create_user_table_query)
-        self.conn.commit()
-        cursor.close()
+    
 
     #generate a user
-    def generate_users(self, firebase_id):
+    def create_user(self, firebase_id, key):
         """
         This function creates a user in the database.
         """
         insert_user_query = """
-        INSERT INTO users (firebase_id)
-        VALUES (%s);
+        INSERT INTO users (firebase_id, keychain)
+        VALUES (%s, %s);
         """
         cursor = self.conn.cursor()
-        cursor.execute(insert_user_query, (firebase_id,))
+        cursor.execute(insert_user_query, (firebase_id, key,))
         self.conn.commit()
         cursor.close()
     
-    #generate a key for a user 
-    def generate_keys(self, platform_code, key, fk_user):
-        """
-        This function creates a key in the database.
-        """
-        insert_user_query = """
-        INSERT INTO keys (platform_code, key, fk_user)
-        VALUES (%s, %s, %s);
-        """
-        cursor = self.conn.cursor()
-        cursor.execute(insert_user_query, (platform_code, key, fk_user))
-        self.conn.commit()
-        cursor.close()
+    
 
 interface = Backend_Interface()
 #interface.create_user_table()
-#interface.create_key_table()
+#interface.create_user(42, "hello")
