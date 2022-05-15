e8866c17ca96f465c5b814c8f9b9360fdfd99bad
Paul Evans <pauljevans927@gmail.com>
2022-05-15 17:01:53 -0400

    fix?

--- src/webscraper/firebase/schedule.py
+++ src/webscraper/firebase/schedule.py
@@ -2,22 +2,13 @@ import json
 
 def write_schedule(user_id, days, db):
 
-    day_array = []
-
-    for key in days:
-        day = days[key]
-        day['day'] = "Day " + str(key)
-        day_array.append(day)
-
     schedule = {
         "user_id": user_id,
-        "days": day_array
+        "days": days
     }
 
     # wierd type issue fix
     schedule = json.loads(json.dumps(schedule))
-
-    print(schedule)
 
     db.collection(u'SCHEDULES').document(f'{user_id}').set(schedule)
 
