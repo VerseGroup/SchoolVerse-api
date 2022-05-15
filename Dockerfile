a8b2961797c624ba46026991bb2526685b16d008
Paul Evans <pauljevans927@gmail.com>
2022-05-15 14:40:57 -0400

    fixing heroku docker bug (doesn't push images properly)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM python:3.9
 
-USER root
-
 WORKDIR /app
 
 RUN pip install --upgrade setuptools
--- docker/heroku.sh
+++ docker/heroku.sh
@@ -1,3 +1,6 @@
+heroku login
+heroku container:login
+
 docker buildx build --platform linux/amd64 -t schoolverse-webscraper .
 docker tag schoolverse-webscraper registry.heroku.com/schoolverse-webscraper/web
 docker push registry.heroku.com/schoolverse-webscraper/web
