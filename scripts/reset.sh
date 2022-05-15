fc7dc1ac2ef1478923d7069fd009813e9178f8f1
Paul Evans <pauljevans927@gmail.com>
2022-05-15 12:21:22 -0400

    adjusting reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -4,7 +4,7 @@ cd lib/resets
 
 COMPLETED="n/a"
 
-while getopts ":ft:" option; do
+while getopts ":fted:" option; do
     case $option in
         f)
             echo "Option: Reset Flik"
@@ -15,6 +15,16 @@ while getopts ":ft:" option; do
             echo "Option: Reset Tasks"
             python3 reset_tasks.py
             COMPLETED="tasks"
+            ;;
+        e)
+            echo "Option: Reset Events"
+            python3 reset_events.py
+            COMPLETED="events"
+            ;;
+        d)
+            echo "Option: Reset Days"
+            python3 reset_days.py
+            COMPLETED="days"
             ;;
         \?) # Invalid option
             echo "Error: Invalid option"
