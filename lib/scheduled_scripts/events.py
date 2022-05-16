4b898927bd84f194030443cab6657c9de88ad1f9
Paul Evans <pauljevans927@gmail.com>
2022-05-16 14:13:11 -0400

    added a script to be scheduled to scrape events daily

---
+++ lib/scheduled_scripts/events.py
@@ -0,0 +1,19 @@
+from src.webscraper.firebase.events import write_events
+from src.webscraper.scraper.veracross import get_events
+from src.webscraper.firebase.auth import start_firebase
+
+from getpass import getpass
+
+def scrape_events(db, username, password):
+    events = get_events(username, password)
+    write_events(events, db)
+
+def script():
+    username = input('Username: ')
+    password = getpass()
+
+    db = start_firebase()
+    scrape_events(db, username, password)
+
+if __name__ == '__main__':
+    script()
