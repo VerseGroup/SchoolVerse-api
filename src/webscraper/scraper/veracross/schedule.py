d9b582a0ba9df4f2f0c2a2c2034177b598318bb1
Paul Evans <pauljevans927@gmail.com>
2022-05-15 22:41:20 -0400

    sorted periods

--- src/webscraper/scraper/veracross/schedule.py
+++ src/webscraper/scraper/veracross/schedule.py
@@ -75,11 +75,20 @@ def parse_html(html) -> list:
         if period_number is None:
             continue
 
+        period_num = period_number
+        period_num = period_num.replace("Period ", "")
+        if period_num == "5a":
+            period_num = 5
+        elif period_num == "5b":
+            period_num = 5.5
+        period_num = float(period_num)
+
         period = {
             "class_name" : class_,
             "information" : teacher,
             "start_time" : time,
             "period" : period_number,
+            "period_num": period_num,
         }
 
         schedule_list.append(period)
@@ -112,6 +121,15 @@ def parse_html(html) -> list:
             time = "8:05am"
 
         schedule_list.append(add_free(period, time))  
+
+    sorted_schedule_list = []
+
+    for i in len(schedule_list):
+        for j in range(len(schedule_list)):
+            if schedule_list[j]['period_num'] < schedule_list[i]['period_num']:
+                sorted_schedule_list.append(schedule_list[j])
+            else:
+                sorted_schedule_list.append(schedule_list[i])
         
     new_schedule_list = {
         "periods" : schedule_list,
