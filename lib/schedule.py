9f7907651a9465ad689fcf8d4e7331c6645dc036
Paul Evans <pauljevans927@gmail.com>
2022-05-17 09:40:12 -0400

    added a new schedule script using the queue

--- lib/schedule.py
+++ lib/schedule.py
@@ -1,60 +1,77 @@
-# THIS SCRIPT CANNOT RUN ON M1 #
-
 # imports
-from vgem import EM
 import os, sys
-import psycopg2
+from vgem import EM
 
 # append paths
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(parent_dir)
 
-# databases
-from src.webscraper.firebase.auth import start_firebase
-from src.webscraper.firebase.credentials import get_encrypted_credentials
-from src.webscraper.firebase.schedule import write_schedule
-from src.postgres.crud import Backend_Interface
-
 # veracross
 from src.webscraper.scraper.veracross.run import scrape_veracross
 
-def scrape_schedule(user_id):
+# firebase
+from src.webscraper.firebase.credentials import get_encrypted_credentials
+from src.webscraper.firebase.schedule import write_schedule
+from src.webscraper.firebase.auth import start_firebase
+
+# postgres
+from src.postgres.crud import Backend_Interface
+
+# This script cannot run on M1, and is designed to be left running #
+
+def update_schedules(db, ss):
+
+    failed = []
+    success = []
     
-    ss = Backend_Interface()
-    db = start_firebase()
+    # get the list of users that need schedules
+    ids = db.collection(u'QUEUES').document(u'schedules_queue').get().to_dict()['user_ids']
 
-    try:
-        key = ss.get_user_keychain(user_id)
-    except:
-        return {"message": "user not in postgres"}
+    for id in ids:
 
-    try:
-        handler = EM(serialized_private_key=key)
-    except:
-        return {"message": "key not valid"}
+        try: 
+            # get user ciphers
+            creds = get_encrypted_credentials(id, 'sc', db)
+            cusername = creds['username_ciphertext']
+            cpassword = creds['password_ciphertext']
 
-    try:
-        creds = get_encrypted_credentials(user_id, 'vc', db)
-        username = creds['username_ciphertext']
-        password = creds['password_ciphertext']
-    except:
-        return {"message": "user not in firebase"}
+            # get user keys
+            key = ss.get_user_keychain(id)
 
-    try: 
-        username = handler.decrypt_rsa(username, True)
-        password = handler.decrypt_rsa(password, True)
-    except:
-        return {"message": "credentials not valid"}
+            # decrypt user ciphers
+            handler = EM(serialized_private_key=key)
+            username = handler.decrypt_rsa(cusername, True)
+            password = handler.decrypt_rsa(cpassword, True)
 
-    try:
-        schedule = scrape_veracross(username, password)
-        write_schedule(schedule, user_id, db)
-    except:
-        return {"message": "schedule not scraped, error with scraper"}
+            # scrape veracross and write to firebase
+            schedule = scrape_veracross(username, password)
+            write_schedule(id, schedule, db)
 
-    return {"message": "success"}
+            # remove user from queue
+            ids.remove(id)
+
+            success.append(id)
+        
+        except Exception as e:
+            failure = {
+                "id": id,
+                "exception": str(e)
+            }
+            failed.append(failure)
+
+    # write queue back to firebase
+    db.collection(u'QUEUES').document(u'schedules_queue').set({'user_ids': ids})
+
+    return {"message" : "finished", "successes": success, "failed": failed}
 
 if __name__ == "__main__":
-    id = input('ID: ')
-    print(scrape_schedule(id))
+    db = start_firebase()
+    ss = Backend_Interface()
+    update_schedules(db, ss)
+    db.close()
+
+
+
+
+
 
