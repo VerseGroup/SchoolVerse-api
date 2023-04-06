76ada72282fdd928681daa84c00a6b8db4dd57cc
Paul Evans <84854052+pevans927@users.noreply.github.com>
2023-04-06 12:31:10 -0400

    new version

--- src/main.py
+++ src/main.py
@@ -578,11 +578,11 @@ async def ping():
 
 @app.get("/version", status_code=200)
 async def version():
-    return {"ios_version": ["1.1.3", '1.2.0']}
+    return {"ios_version": ['1.2.0', '1.2.1']}
 
 @app.get("/version2", status_code=200)
 async def version2():
-    return {"ios_version": ["1.1.3", '1.2.0']}
+    return {"ios_version": ['1.2.0', '1.2.1']}
 
 @app.get("/getexecutions", status_code=200)
 async def get_executions():
